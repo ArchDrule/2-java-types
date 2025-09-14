72f8062f57d80fe8ef4d6e9e8b0bffa5abdb9a3d
Георгий <georg.plotnikov03@gmail.com>
2025-09-14 21:33:23 +0500

    Плотников Георгий ПрИ-202 — Задание 5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,19 +3,21 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
+        String str = Integer.toString(x);
+        for (int i = 0; i < str.length(); i++) {
+            int num = str.charAt(i) - 'a' + 1;
+            if (num % 2 != 0)
+                return "FALSE";
+        }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         String result = solution(1234);
         System.out.println(result);
-        */
     }
 
 }
