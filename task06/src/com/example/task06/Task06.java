fd0dc114e2a59d63573f4f2fc3529a1f44631ff0
Георгий <georg.plotnikov03@gmail.com>
2025-09-14 21:42:59 +0500

    Плотников Георгий ПрИ-202 — Задание 6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,18 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = x + y;
+        String str = Integer.toString(sum);
+        if (str.contains("-"))
+            str = str.substring(1);
+        return str.length();
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
 
 }
